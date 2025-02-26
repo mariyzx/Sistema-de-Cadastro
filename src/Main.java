775f33efef52b7d74e423e6c39208985d08df157
Mariana Werneck <marinhomariana8@gmail.com>
2025-02-26 19:33:21 -0300

    add menu

--- src/Main.java
+++ src/Main.java
@@ -112,7 +112,7 @@ public class Main {
             System.out.println("2 - Listar todos os usuários cadastrados");
             System.out.println("3 - Cadastrar nova pergunta no formulário");
             System.out.println("4 - Deletar pergunta do formulário");
-            System.out.println("5 - Pesquisar usuário por nome ou email");
+            System.out.println("5 - Pesquisar usuário por nome ou email ");
 
             int menuOption = menu.nextInt();
             menu.nextLine();
